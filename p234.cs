de647cc7e6756054495579d9414b114b640ca068
PashaAllahverdiyev <pashama@code.edu.az>
2023-04-02 21:09:11 +0400

    p234.cs

---
+++ p234.cs
@@ -0,0 +1,9 @@
+﻿int[] numbers = { 3, 4, 8, 16, 27, 35 };
+int best = numbers[0];
+for (int i = 0; i < numbers.Length; i++) 
+{
+    if (numbers[i] > best) ;
+    best = numbers[i];
+
+}
+Console.WriteLine(best);
